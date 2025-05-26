25578ac2c3d46e4592b0fb50a809221178a0b4af
Pinyituri1 <jfernandotrujillo@ucundinamarca.edu.co>
2025-05-26 16:41:42 -0500

    first commit

--- src/main/java/com/example/BDPostgres/Repository/EquipoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.BDPostgres.Repository;
-
-import com.example.BDPostgres.Model.Equipo;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EquipoRepository extends JpaRepository<Equipo, Integer> {
-}
